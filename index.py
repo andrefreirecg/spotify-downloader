137a66612d0457773b5efa4a80ec71a89f7ddfb3
Andre Freire <andrefreirecg@gmail.com>
2023-05-07 17:35:49 -0300

    first_commit

---
+++ index.py
@@ -0,0 +1,20 @@
+import os, re, requests
+from bs4 import BeautifulSoup
+
+current_path = os.path.abspath(os.path.dirname(__file__))
+links = [
+    "https://open.spotify.com/playlist/0QYQbFTkVVj2DxYG3HEw15",
+]
+
+for link in links:
+    response = requests.get(link)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    album_name = soup.title.string.split(',')[0].strip().replace(' ', '-')
+    folder_name = f"{album_name}/"
+    if not os.path.exists(folder_name):
+        os.mkdir(folder_name)
+    os.chdir(folder_name)
+
+    os.system(f'python3 -m spotdl {link}')
+    os.chdir(current_path)
+
